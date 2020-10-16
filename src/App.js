efa4bec6e408a3cf75d8e251f79fac9d2b4153a8
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-10-16 16:00:56 -0700

    web screenshot app using flashApi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [img, setImg] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setUrl('');
+    setImg('');
+    setErr('');
+    setLoading(true)
+    axios.get(`https://api.apiflash.com/v1/urltoimage?access_key=6ed355c3c886479fb4d20bd8de27ede2&url=${url}&fresh=true&full_page=true`)
+    .then(res => {
+      setLoading(false);
+      setImg(res.config.url)
+    })
+    .catch(error => {
+      setLoading(false);
+      setErr(true)
+    })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav-container'>
+        <div className='nav'>
+            <form onSubmit={handleSubmit}>
+              <input type='text' className='url' value={url} onChange={(e) => setUrl(e.target.value)} />
+              <button className='btn' type='submit'>Take ScreenShot</button>
+            </form>
+        </div>
+      </div>
+      <div className='container'>
+        {
+          img && <img src={img} alt='Screenshot'/>
+        }
+        {
+          err && <p>Please Enter thr valid Url or check your internet</p>
+        }
+        {
+          loading && <p>loading....</p>
+        }
+      </div>
     </div>
   );
 }
